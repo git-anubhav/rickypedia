96340bde6bdcd9ad85c9c58210f40fb0e830541c
git-anubhav <anubhxv@gmail.com>
2021-02-15 19:57:52 +0530

    link correct

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -18,7 +18,7 @@ export default function Landing() {
           <Link to="/characters">
             <div className="option">
               <img
-                src="./rickypedia/images/landing/characters.jpg"
+                src="./images/landing/characters.jpg"
                 className="landing-images"
                 alt="characters"
               />
@@ -28,7 +28,7 @@ export default function Landing() {
           <Link to="/episodes">
             <div className="option">
               <img
-                src="./rickypedia/images/landing/episodes.jpg"
+                src="./images/landing/episodes.jpg"
                 className="landing-images"
                 alt="episodes"
               />
